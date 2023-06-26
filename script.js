3fd7af8e866d1e97d9417834a746fdd2e893f76e
KaiWiehe <Kai_Wiehe@live.de>
2023-06-26 16:03:13 +0200

    month changed from German to English

--- script.js
+++ script.js
@@ -5,55 +5,59 @@ contacts = [];
 categorys = [];
 
 let today = new Date();
-let monthL = new Date().toLocaleString('de-de', { month: 'long' });
+let monthL = new Date().toLocaleString("en-en", { month: "long" });
 
 async function init() {
-    await downloadBackend();
-    await includeHTML();
-    changeImg();
-    loadTasks();
-    loadContacts();
-    sayHello();
+	await downloadBackend();
+	await includeHTML();
+	changeImg();
+	loadTasks();
+	loadContacts();
+	sayHello();
 }
 
 async function initLogin() {
-    await downloadBackend();
-    await includeHTML();
-    loadMSG();
+	await downloadBackend();
+	await includeHTML();
+	loadMSG();
 }
 
 /** Inserts the data from the JSON into the summary, into the board*/
 function loadTasks() {
-    clearAllDivs();
-    loadSummary();
-    tasks.forEach((task, index) => loadBoard(task, index));
-    backend.setItem('tasks', JSON.stringify(tasks));
+	clearAllDivs();
+	loadSummary();
+	tasks.forEach((task, index) => loadBoard(task, index));
+	backend.setItem("tasks", JSON.stringify(tasks));
 }
 
 /** Changes the two images depending on the logged in profile */
 function changeImg() {
-    let userImgHeader = document.getElementById('userImgHeader');
-    let userImg = document.getElementById('userImg');
+	let userImgHeader = document.getElementById("userImgHeader");
+	let userImg = document.getElementById("userImg");
 
-    if (activeUser.img != 'noImg') {
-        userImgHeader.innerHTML = `<img class="userImgHeader" src="${activeUser.img}" alt="Profile Img">`;
-        userImg.innerHTML = `<img src="${activeUser.img}" alt="user img">`;
-    } else {
-        userImgHeader.innerHTML = `<div class="noImg" id="noImg">${firstLetter(activeUser.name)}</div>`;
-        userImg.innerHTML = `<div class="noImg" id="noImg" style="width: 5rem; height: 5rem;">${firstLetter(activeUser.name)}</div>`;
-    }
+	if (activeUser.img != "noImg") {
+		userImgHeader.innerHTML = `<img class="userImgHeader" src="${activeUser.img}" alt="Profile Img">`;
+		userImg.innerHTML = `<img src="${activeUser.img}" alt="user img">`;
+	} else {
+		userImgHeader.innerHTML = `<div class="noImg" id="noImg">${firstLetter(
+			activeUser.name
+		)}</div>`;
+		userImg.innerHTML = `<div class="noImg" id="noImg" style="width: 5rem; height: 5rem;">${firstLetter(
+			activeUser.name
+		)}</div>`;
+	}
 }
 
 function doNotClose(event) {
-    event.stopPropagation();
+	event.stopPropagation();
 }
 
 function banner(string, style, containerID, timeout) {
-    let container = document.getElementById(containerID);
-    container.innerHTML = string;
-    container.style = style;
-    container.classList.remove('hide');
-    setTimeout(() => container.classList.add('hide'), timeout);
+	let container = document.getElementById(containerID);
+	container.innerHTML = string;
+	container.style = style;
+	container.classList.remove("hide");
+	setTimeout(() => container.classList.add("hide"), timeout);
 }
 
 /**
@@ -61,15 +65,20 @@ function banner(string, style, containerID, timeout) {
  * @returns Array without dublicates
  */
 function arrClean(arr, errorText, containerID) {
-    const data = arr;
-    const set = new Set(data.map((item) => JSON.stringify(item)));
-    const dedup = [...set].map((item) => JSON.parse(item));
-    if (data.length - dedup.length > 0) {
-        //if contact already exists
-        banner(errorText, 'background: rgba(255, 0, 0, 0.538);', containerID, 1250);
-        alreadyExists = true;
-    }
-    return dedup;
+	const data = arr;
+	const set = new Set(data.map((item) => JSON.stringify(item)));
+	const dedup = [...set].map((item) => JSON.parse(item));
+	if (data.length - dedup.length > 0) {
+		//if contact already exists
+		banner(
+			errorText,
+			"background: rgba(255, 0, 0, 0.538);",
+			containerID,
+			1250
+		);
+		alreadyExists = true;
+	}
+	return dedup;
 }
 
 /**
@@ -80,7 +89,7 @@ function arrClean(arr, errorText, containerID) {
  * //console.log(capitalizeFirstLetter(name))
  */
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 /**
@@ -91,43 +100,45 @@ function capitalizeFirstLetter(string) {
  * //console.log(capitalizeFirstLetter(name))
  */
 function firstLetter(string) {
-    return string.charAt(0).toUpperCase();
+	return string.charAt(0).toUpperCase();
 }
 
 function clearAllDivs() {
-    let tasksInBoard = document.getElementById('tasksInBoard');
-    let tasksInProgress = document.getElementById('tasksInProgress');
-    let awaitingFeedbackSummary = document.getElementById('awaitingFeedbackSummary');
-    let todoSummary = document.getElementById('todoSummary');
-    let doneSummary = document.getElementById('doneSummary');
+	let tasksInBoard = document.getElementById("tasksInBoard");
+	let tasksInProgress = document.getElementById("tasksInProgress");
+	let awaitingFeedbackSummary = document.getElementById(
+		"awaitingFeedbackSummary"
+	);
+	let todoSummary = document.getElementById("todoSummary");
+	let doneSummary = document.getElementById("doneSummary");
 
-    tasksInBoard.innerHTML = ``;
-    tasksInProgress.innerHTML = ``;
-    awaitingFeedbackSummary.innerHTML = ``;
-    todoSummary.innerHTML = ``;
-    doneSummary.innerHTML = ``;
+	tasksInBoard.innerHTML = ``;
+	tasksInProgress.innerHTML = ``;
+	awaitingFeedbackSummary.innerHTML = ``;
+	todoSummary.innerHTML = ``;
+	doneSummary.innerHTML = ``;
 
-    let todo = document.getElementById('todo');
-    let inProgress = document.getElementById('inProgress');
-    let awaitingFeedback = document.getElementById('awaitingFeedback');
-    let done = document.getElementById('done');
+	let todo = document.getElementById("todo");
+	let inProgress = document.getElementById("inProgress");
+	let awaitingFeedback = document.getElementById("awaitingFeedback");
+	let done = document.getElementById("done");
 
-    todo.innerHTML = ``;
-    inProgress.innerHTML = ``;
-    awaitingFeedback.innerHTML = ``;
-    done.innerHTML = ``;
+	todo.innerHTML = ``;
+	inProgress.innerHTML = ``;
+	awaitingFeedback.innerHTML = ``;
+	done.innerHTML = ``;
 }
 
 /** empties all containers (todo, inProgress, awaitingFeedback, done) */
 function clearBoard() {
-    let todo = document.getElementById('todo');
-    let inProgress = document.getElementById('inProgress');
-    let awaitingFeedback = document.getElementById('awaitingFeedback');
-    let done = document.getElementById('done');
-    todo.innerHTML = ``;
-    inProgress.innerHTML = ``;
-    awaitingFeedback.innerHTML = ``;
-    done.innerHTML = ``;
+	let todo = document.getElementById("todo");
+	let inProgress = document.getElementById("inProgress");
+	let awaitingFeedback = document.getElementById("awaitingFeedback");
+	let done = document.getElementById("done");
+	todo.innerHTML = ``;
+	inProgress.innerHTML = ``;
+	awaitingFeedback.innerHTML = ``;
+	done.innerHTML = ``;
 }
 
 /**
@@ -135,42 +146,45 @@ function clearBoard() {
  * @returns {JSON} - the selected task
  */
 function returnSelectedTask(i) {
-    let id = [i];
-    let task = tasks.filter((itm) => {
-        return id.indexOf(Number(itm.id)) > -1;
-    });
-    task = task[0];
-    return task;
+	let id = [i];
+	let task = tasks.filter((itm) => {
+		return id.indexOf(Number(itm.id)) > -1;
+	});
+	task = task[0];
+	return task;
 }
 
 /** opens the say hello banner */
 function sayHello() {
-    let container = document.getElementById('categoryAlreadyExistsContainer');
-    container.innerHTML += `Good Morning ${activeUser.name}. It's ${monthL} ${today.getDate()}, ${today.getFullYear()}. Have a nice Day.`;
-    container.style = 'background: var(--leftGrey); padding: 4rem 1rem 1rem 1rem; width: 80%;';
-    container.classList.remove('hide');
-    setTimeout(() => container.classList.add('hide'), 10000);
+	let container = document.getElementById("categoryAlreadyExistsContainer");
+	container.innerHTML += `Good Morning ${
+		activeUser.name
+	}. It's ${monthL} ${today.getDate()}, ${today.getFullYear()}. Have a nice Day.`;
+	container.style =
+		"background: var(--leftGrey); padding: 4rem 1rem 1rem 1rem; width: 80%;";
+	container.classList.remove("hide");
+	setTimeout(() => container.classList.add("hide"), 10000);
 }
 
 /** close the say hello banner */
 function closeSayHello() {
-    let container = document.getElementById('categoryAlreadyExistsContainer');
-    container.classList.add('hide');
+	let container = document.getElementById("categoryAlreadyExistsContainer");
+	container.classList.add("hide");
 }
 
 /**
- * switch the category 
+ * switch the category
  * @param {number} number - the number of the respective menu item
  */
 function switchCategorys(number) {
-    new SwitchCategorys(number);
+	new SwitchCategorys(number);
 }
 
 /**
  * @returns {HTMLElement} - the add task HTML snippet
  */
 function addTaskHTML() {
-    return /* html */ `
+	return /* html */ `
 <h1 id="addTaskh1">Add Task</h1>
 <form class="addTaskForm" id="addTaskForm" onsubmit="return false;" >
     <div class="addTaskLeft">
@@ -233,15 +247,15 @@ function addTaskHTML() {
  * @param {HTMLElement} el
  */
 function clearValue(el) {
-    el.value = '';
+	el.value = "";
 }
 
 /**
- * 
- * @param {string} string1 
- * @param {string} string2 
- * @returns {boolean} - true = same | false = different 
+ *
+ * @param {string} string1
+ * @param {string} string2
+ * @returns {boolean} - true = same | false = different
  */
 function sameString(string1, string2) {
-    return string1 === string2;
+	return string1 === string2;
 }
